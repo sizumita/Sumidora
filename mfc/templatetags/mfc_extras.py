acd2e48d2087fb8e5a8b353c8c1b79d33cacf535
sizumita <sumito@izumita.com>
2017-10-07 12:51:40 +0900

    自作タグを使わないようにした

--- mfc/templatetags/mfc_extras.py
+++ mfc/templatetags/mfc_extras.py
@@ -1,12 +1,12 @@
 from django import template
-
 register = template.Library()
 
 @register.filter(name="win", is_safe=True, needs_autoescape=True)
+@register.inclusion_tag('mfc.html')
 def win(uuid,uuid2,name1,name2):
-    if uuid == uuid2:
+    if str(uuid) == str(uuid2):
         return name1
     else:
         return name2
 
-# register.filter('win', win)
+register.filter('win', win)
